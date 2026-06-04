8eac76cbef5e7ebc5951bbbce518c2b7c1ca7265
Isha Shinde <149763675+Isha-Shinde@users.noreply.github.com>
2026-06-04 20:54:52 +0530

    Add files via upload

---
+++ C_Programming/Practice/program17.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEvenOdd(int iNo)
+{
+    if((iNo % 2) == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false; //internally it is false(0)
+
+    printf("Enter number to check wheather it is even or odd : ");
+    scanf("%d",&iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+    if(bRet)
+    {
+        printf("%d is Even \n",iValue);
+    }
+    else
+    {
+        printf("%d is Odd\n",iValue);
+    }
+    return 0;
+}
