d7f271730158a561227615567a1caffb79aaa83f
Isha Shinde <149763675+Isha-Shinde@users.noreply.github.com>
2026-06-09 08:35:46 +0530

    Add files via upload

---
+++ C_Programming/Competitive/set_15/que1.c
@@ -0,0 +1,75 @@
+//Accept N numbers from user and accept one another number as NO,check wheather No is present or not.
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define FALSE 0
+#define TRUE 1
+
+typedef int BOOL;
+
+BOOL Check(int Arr[],int iLength,int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0;iCnt < iLength;iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            break;
+        }
+    }
+    if(iCnt < iLength)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+int main()
+{
+    int iSize = 0,iCnt = 0,iValue = 0;
+    int *p = NULL;
+    BOOL bRet = FALSE;
+
+    printf("Enter number of elements :");
+    scanf("%d",&iSize);
+
+    
+    printf("Enter the number : \n");
+    scanf("%d",&iValue);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to alloacte memory");
+        return -1;
+    }
+
+    printf("Enters %d elements \n",iSize);
+
+    for(iCnt = 0;iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d :",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    bRet = Check(p,iSize,iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("Number is present");
+    }
+    else
+    {
+        printf("Number is not present");
+    }
+
+    free(p);
+
+    return 0;
+}
---
+++ C_Programming/Competitive/set_16/que5.c
@@ -0,0 +1,57 @@
+//Accept N numbers froom user and display summation of digits of each number.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void Digits(int Arr[],int iLength)
+{
+    int iCnt = 0,iNo = 0,iDigit = 0;
+    int iSum = 0;
+
+    while(iCnt < iLength)
+    {
+        iNo = Arr[iCnt];
+        iSum = 0;
+
+        while(iNo != 0)
+        {
+           iDigit = iNo % 10;
+           iSum = iSum + iDigit;
+           iNo = iNo / 10;
+        }
+        printf("Summation of digit of %d is : %d\n", Arr[iCnt],iSum);
+
+        iCnt++;
+    }
+}
+int main()
+{
+    int iSize = 0,iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements :");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements\n",iSize);
+
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        printf("Enter element %d :",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    Digits(p,iSize);
+
+    free(p);
+
+    return 0;
+
+}
