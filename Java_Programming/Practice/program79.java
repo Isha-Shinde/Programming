58809ede5a30cfdcb9560ee019af4da8d9e03d30
Isha Shinde <149763675+Isha-Shinde@users.noreply.github.com>
2026-06-15 14:10:13 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program79.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+class program79 {
+
+    public static void main(String[] A) {
+
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+
+        System.out.println("Enter Number :");
+        iValue = sobj.nextInt();
+
+        dobj.DisplayDigits(iValue);
+
+    }
+}
+
+class DigitX {
+
+    public void DisplayDigits(int iNo) {
+        int iDigit = 0;
+        iDigit = 1;
+
+        while (iDigit != 0) {
+            iDigit = iNo % 10;
+            {
+                System.out.println(iDigit);
+            }
+            iNo = iNo / 10;
+
+        }
+    }
+}
