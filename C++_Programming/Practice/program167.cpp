5b82969b611bb99b11748f40880a495c971a8aa7
Isha Shinde <149763675+Isha-Shinde@users.noreply.github.com>
2026-07-01 21:53:11 +0530

    Add files via upload

---
+++ C++_Programming/Practice/program167.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    private:
+        int *Arr;
+        int iSize;
+        
+    public:
+    // parameterised constructor with default argument
+        ArrayX(int X = 5)
+        {
+            iSize = X;              
+            Arr = new int[iSize];   
+        }
+        
+        ~ArrayX()
+        {
+            delete []Arr;      
+        }
+};
+
+int main()
+{
+
+    ArrayX *aobj1 = new ArrayX();        //parameterised constructor
+    ArrayX *aobj2 = new ArrayX(15);       //parameterised constructor
+
+    //function call
+
+    delete aobj1;
+    delete aobj2;
+
+    return 0;
+
+}
+    
---
+++ C++_Programming/Practice/program168.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    private:
+        int *Arr;
+        int iSize;
+        
+    public:
+    // parameterised constructor with default argument
+        ArrayX(int X = 5)
+        {
+            iSize = X;              
+            Arr = new int[iSize];   
+        }
+        
+        ~ArrayX()
+        {
+            delete []Arr;      
+        }
+
+        void accept()
+        {
+            int iCnt = 0;
+            cout<<"Enter the elements : \n";
+
+            for(iCnt = 0;iCnt <iSize;iCnt++)
+            {
+                cin>>Arr[iCnt];
+            }
+        }
+
+        void Display()
+        {
+            int iCnt = 0;
+            cout<<"Elements of the array are : \n";
+
+            for(iCnt = 0;iCnt <iSize;iCnt++)
+            {
+                cout<<Arr[iCnt]<<endl;
+            }
+        }
+};
+
+int main()
+{
+
+    ArrayX *aobj = NULL;
+
+    int iLength = 0;
+    
+    cout<<"Enter the number of elements : \n";
+    cin>>iLength;
+
+    aobj = new ArrayX[iLength];
+
+    aobj->accept();
+    aobj->Display();
+
+    delete aobj;
+
+    return 0;
+
+}
+    
