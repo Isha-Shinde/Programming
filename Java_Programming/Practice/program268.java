9c0154886bf653e208fc99dd7c06747dd615bd06
Isha Shinde <149763675+Isha-Shinde@users.noreply.github.com>
2026-07-24 22:51:30 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program268.java
@@ -0,0 +1,24 @@
+import java .util.*;
+
+class program268
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String str = null;
+        StringX strobj = new StringX();
+
+        System.out.println("Enter String :");
+        str = sobj.nextLine();
+
+        strobj.Display(str);
+    }
+}
+
+class StringX
+{
+    public void Display(String str)
+    {
+        System.out.println("received string is :"+str);
+    }
+}
