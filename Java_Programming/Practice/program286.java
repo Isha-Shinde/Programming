268ca451a971e834a010d76038f60b7db03413f8
Isha Shinde <149763675+Isha-Shinde@users.noreply.github.com>
2026-07-26 22:19:56 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program286.java
@@ -0,0 +1,37 @@
+import java .util.*;
+
+class program286
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+        StringX strobj = new StringX();
+        String sRet = null;
+
+        System.out.println("Enter String :");
+        data = sobj.nextLine();
+        
+        sRet = strobj.toUpperX(data);
+        System.out.println("Updated string is :"+sRet);
+    }
+}
+
+class StringX
+{
+    public String toUpperX(String str)
+    {
+       char Arr[] = str.toCharArray();
+       int i = 0;
+
+       for(i = 0; i < Arr.length; i++)
+       {
+        if(Arr[i] >= 'a' && Arr[i] <= 'z')
+        {
+            Arr[i] = (char)(Arr[i] - 32);   
+        }
+          
+       }
+       return new String(Arr);         
+    }
+}
---
+++ Java_Programming/Practice/program288.java
@@ -0,0 +1,80 @@
+import java .util.*;
+
+class program288
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+        StringX strobj = new StringX();
+        String sRet = null;
+
+        System.out.println("Enter String :");
+        data = sobj.nextLine();
+        
+        
+        sRet = strobj.toUpperX(data);
+        System.out.println("Updated string is :"+sRet);
+
+        sRet = strobj.toLower(data);
+        System.out.println("Updated string is :"+sRet);
+
+        sRet = strobj.Toggle(data);
+        System.out.println("Updated string is :"+sRet);
+    }
+}
+
+class StringX
+{
+    
+    public String toUpperX(String str)
+    {
+       char Arr[] = str.toCharArray();
+       int i = 0;
+
+       for(i = 0; i < Arr.length; i++)
+       {
+        if(Arr[i] >= 'a' && Arr[i] <= 'z')
+        {
+            Arr[i] = (char)(Arr[i] - 32);   
+        }
+          
+       }
+       return new String(Arr);         
+    }
+
+    public String toLower(String str)
+    {
+       char Arr[] = str.toCharArray();
+       int i = 0;
+
+       for(i = 0; i < Arr.length; i++)
+       {
+        if(Arr[i] >= 'A' && Arr[i] <= 'Z')
+        {
+            Arr[i] = (char)(Arr[i] + 32);   
+        }
+          
+       }
+       return new String(Arr);         
+    }
+
+    public String Toggle(String str)
+    {
+       char Arr[] = str.toCharArray();
+       int i = 0;
+
+       for(i = 0; i < Arr.length; i++)
+       {
+        if(Arr[i] >= 'A' && Arr[i] <= 'Z')
+        {
+            Arr[i] = (char)(Arr[i] + 32);   
+        }
+        else   //isssue
+        {
+            Arr[i] = (char)(Arr[i] - 32); 
+        }     
+       }
+       return new String(Arr);         
+    }
+}
