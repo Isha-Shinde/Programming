e954f15314a44472ee4ebf275ab2d90ca50335dc
Isha Shinde <149763675+Isha-Shinde@users.noreply.github.com>
2026-08-14 10:19:07 +0530

    Add files via upload

---
+++ C++_Programming/Practice/program566.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Searching
+{
+    private :
+        int *Arr;
+        int iSize;
+
+    public:
+        Searching(int iNo);
+        ~Searching();
+
+        void Accept();
+        void Display();
+};
+
+Searching :: Searching(int iNo)
+{
+    iSize = iNo;
+    Arr = new int[iSize];
+}
+
+Searching :: ~Searching()
+{
+    delete []Arr;
+}
+
+void Searching :: Accept()
+{
+    int i = 0;
+   cout<<"enter the elements : \n";
+
+   for(i = 0; i < iSize; i++)
+   {
+      cin>>Arr[i];
+   }
+}
+
+void Searching :: Display()
+{
+    int i = 0;
+   cout<<"elements of the array are : \n";
+
+   for(i = 0; i < iSize; i++)
+   {
+      cout<<Arr[i]<<"\n";
+   }
+}
+
+int main()
+{
+    Searching sobj(5);
+
+    sobj.Accept();
+    sobj.Display();
+
+    return 0;
+}
